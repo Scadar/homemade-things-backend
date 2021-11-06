33fd6f7501eec71754268cc1edb6d33c73d9317a
Roman Miroshnikov <ramiroshnikov@vtb.ru>
2021-11-06 10:10:17 +0300

    Spring security with jwt tokens

---
+++ src/main/java/ru/homemadethings/homemadethings/HomemadeThingsApplication.java
@@ -0,0 +1,19 @@
+package ru.homemadethings.homemadethings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+        HomemadeThingsApplication.class,
+        Jsr310JpaConverters.class
+})
+public class HomemadeThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomemadeThingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/homemadethings/homemadethings/HomemadeThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homemadethings.homemadethings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemadeThingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
